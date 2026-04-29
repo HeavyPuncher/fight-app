73ad780f0e662e06c655d8315a9e9b7b441c1b61
Ali Fawzi <ali_fawzi@hotmail.co.uk>
2026-04-29 18:59:03 +0100

    Added more data

--- src/data/fights.js
+++ src/data/fights.js
@@ -1,44 +1,155 @@
 // src/data/fights.js
 export const fights = [
-    {
-      id: 1,
-      event: "THE DAY",
-      fighters: ["Naoya Inoue", "Junto Nakatani"],
-      date: "02-05-2026",
-      location: "Tokyo, Japan",
-      type: "boxing"
-    },
-    {
-      id: 2,
-      event: "UFC FIGHT NIGHT: DELLA MADDALENA VS PRATES",
-      fighters: ["JackDella Maddalena", "Carlos Prates"],
-      date: "02-05-2026",
-      location: "Perth, Australia",
-      type: "ufc"
-    },
-    {
-      id: 3,
-      event: "BEAUTY VS THE BEAST",
-      fighters: ["TOMMY FURY", "EDDIE HALL"],
-      date: "13-06-2026",
-      location: "Manchester, UK",
-      type: "influencer"
-    },
-    
-    {
-      id: 4,
-      event: "Benavidez vs Ramirez",
-      fighters: ["David Benavidez", "Gilberto 'Zurdo' Ramirez"],
-      date: "02-05-2026",
-      location: "Las Vegas, USA",
-      type: "boxing"
-    },
-    {
-      id: 5,
-      event: "Wardley vs Dubois",
-      fighters: ["Fabio Wardley", "Daniel Dubois"],
-      date: "09-05-2026",
-      location: "Manchester, UK",
-      type: "boxing"
-    }
-  ];
+  {
+    id: 1,
+    event: "THE DAY: Inoue vs Nakatani",
+    fighters: ["Naoya Inoue", "Junto Nakatani"],
+    date: "02-05-2026",
+    location: "Tokyo, Japan",
+    type: "boxing"
+  },
+  {
+    id: 2,
+    event: "UFC Perth: Della Maddalena vs Prates",
+    fighters: ["Jack Della Maddalena", "Carlos Prates"],
+    date: "02-05-2026",
+    location: "Perth, Australia",
+    type: "ufc"
+  },
+  {
+    id: 3,
+    event: "Ramirez vs Benavidez",
+    fighters: ["Gilberto Ramirez", "David Benavidez"],
+    date: "02-05-2026",
+    location: "Las Vegas, USA",
+    type: "boxing"
+  },
+  {
+    id: 4,
+    event: "Wardley vs Dubois",
+    fighters: ["Fabio Wardley", "Daniel Dubois"],
+    date: "09-05-2026",
+    location: "Manchester, UK",
+    type: "boxing"
+  },
+  {
+    id: 5,
+    event: "UFC 328: Chimaev vs Strickland",
+    fighters: ["Khamzat Chimaev", "Sean Strickland"],
+    date: "09-05-2026",
+    location: "Newark, NJ, USA",
+    type: "ufc"
+  },
+  {
+    id: 6,
+    event: "Leo vs Aleem",
+    fighters: ["Angelo Leo", "Raeese Aleem"],
+    date: "09-05-2026",
+    location: "Atlanta, USA",
+    type: "boxing"
+  },
+  {
+    id: 7,
+    event: "Mosley Jr vs Bohachuk",
+    fighters: ["Shane Mosley Jr", "Serhii Bohachuk"],
+    date: "10-05-2026",
+    location: "Las Vegas, USA",
+    type: "boxing"
+  },
+  {
+    id: 8,
+    event: "UFC Fight Night: Allen vs Costa",
+    fighters: ["Arnold Allen", "Borrachinha Costa"],
+    date: "16-05-2026",
+    location: "Las Vegas, NV, USA",
+    type: "ufc"
+  },
+  {
+    id: 9,
+    event: "Davis vs Albright",
+    fighters: ["Keyshawn Davis", "Nahir Albright"],
+    date: "16-05-2026",
+    location: "Norfolk, USA",
+    type: "boxing"
+  },
+  {
+    id: 10,
+    event: "Usyk vs Verhoeven",
+    fighters: ["Oleksandr Usyk", "Rico Verhoeven"],
+    date: "23-05-2026",
+    location: "Giza, Egypt",
+    type: "boxing"
+  },
+  {
+    id: 11,
+    event: "UFC Fight Night: Song vs Figueiredo",
+    fighters: ["Song Yadong", "Deiveson Figueiredo"],
+    date: "30-05-2026",
+    location: "Macao",
+    type: "ufc"
+  },
+  {
+    id: 12,
+    event: "Foster vs Ford",
+    fighters: ["O'Shaquie Foster", "Raymond Ford"],
+    date: "30-05-2026",
+    location: "Houston, USA",
+    type: "boxing"
+  },
+  {
+    id: 13,
+    event: "Bivol vs Eifert",
+    fighters: ["Dmitry Bivol", "Michael Eifert"],
+    date: "30-05-2026",
+    location: "Yekaterinburg, Russia",
+    type: "boxing"
+  },
+  {
+    id: 14,
+    event: "UFC Fight Night: Muhammad vs Bonfim",
+    fighters: ["Belal Muhammad", "Gabriel Bonfim"],
+    date: "06-06-2026",
+    location: "Las Vegas, NV, USA",
+    type: "ufc"
+  },
+  {
+    id: 15,
+    event: "Sandoval vs Yafai",
+    fighters: ["Ricardo Rafael Sandoval", "Galal Yafai"],
+    date: "06-06-2026",
+    location: "Sheffield, UK",
+    type: "boxing"
+  },
+  {
+    id: 16,
+    event: "Rodriguez vs Vargas",
+    fighters: ["Jesse 'Bam' Rodriguez", "Antonio Vargas"],
+    date: "13-06-2026",
+    location: "Glendale, USA",
+    type: "boxing"
+  },
+  {
+    id: 17,
+    event: "BEAUTY VS THE BEAST: Fury vs Hall",
+    fighters: ["Tommy Fury", "Eddie Hall"],
+    date: "13-06-2026",
+    location: "Manchester, UK",
+    type: "influencer"
+  },
+  {
+    id: 18,
+    event: "Mayweather Jr vs Zambidis",
+    fighters: ["Floyd Mayweather Jr", "Mike Zambidis"],
+    date: "13-06-2026",
+    location: "Athens, Greece",
+    type: "influencer"
+  },
+  {
+    id: 19,
+    event: "UFC Freedom 250",
+    fighters: ["Illia Topuria", "Justin Gaethje"],
+    date: "14-06-2026",
+    location: "Washington, DC, USA",
+    type: "ufc"
+  }
+];
