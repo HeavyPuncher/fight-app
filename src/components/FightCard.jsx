a1ef2a85fca4d1db5415ca5ca2b34a7777c647f4
Ali Fawzi <ali_fawzi@hotmail.co.uk>
2026-05-12 02:03:04 +0100

    Removing commented code

--- src/components/FightCard.jsx
+++ src/components/FightCard.jsx
@@ -1,27 +1,3 @@
-// import { useState } from "react";
-
-// function FightCard({ fight }) {
-//     const [showUndercard, setShowUndercard] = useState(false);
-
-//     return (
-//       <div onClick={() => setShowUndercard(!showUndercard)} style={{
-//         border: "1px solid #ccc",
-//         borderRadius: "10px",
-//         padding: "16px",
-//         marginBottom: "12px"
-//       }}>
-//         <h3>{fight.event}</h3>
-//         <p><strong>{fight.fighters.join(" vs ")}</strong></p>
-//         <p>Date: {fight.date}</p>
-//         <p>Location: {fight.location}</p>
-//         <p>{fight.type.toUpperCase()}</p>
-//       </div>
-//     );
-//   }
-
-// export default FightCard;
-import { useState } from "react";
-
 function FightCard({ fight }) {
   const [showUndercard, setShowUndercard] = useState(false);
 
